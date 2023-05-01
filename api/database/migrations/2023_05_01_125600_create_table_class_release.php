7cc2cd846e43945f0c08417d7a93038a2a47c50e
Gusttavo <eg9653641@gmail.com>
2023-05-01 10:04:45 -0300

    Migration da tabela Class_Release

---
+++ api/database/migrations/2023_05_01_125600_create_table_class_release.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('class_release', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('request_release')->cascadeOnDelete()->constrained(
+                table: 'request_release', indexName: 'fk_class_release_request_release'
+            );
+            $table->foreignId('class')->cascadeOnDelete()->constrained(
+                table: 'class', indexName: 'fk_class_release_class'
+            );
+            $table->foreignId('release_status')->cascadeOnDelete()->constrained(
+                table: 'release_status', indexName: 'fk_class_release_release_status'
+            );
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('class_release');
+    }
+};
