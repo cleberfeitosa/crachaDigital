651f0b32b70b0ac1cabbbc61cd0bf7774ff12f6a
Gusttavo <eg9653641@gmail.com>
2023-04-30 21:43:05 -0300

    feat: Migration da tabela course.

---
+++ api/database/migrations/2023_04_30_234041_create_table_course.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('course', function (Blueprint $table) {
+            $table->id();
+            $table->string("course_code", 25)->unique();
+            $table->string("course_name", 50);
+            /**
+             * 1° forma
+             *  $table->bigInteger('course_type');
+             *  $table->foreign('course_type')
+             *      ->references('id')
+             *      ->on('course_type')
+             *      ->cascadeOnDelete();
+             */
+            $table->foreignId('course_type')->cascadeOnDelete()->constrained(
+                table: 'course_type', indexName: 'fk_course_course_type'
+            );
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('course');
+    }
+};
