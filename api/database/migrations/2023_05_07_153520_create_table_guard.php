a35292ce6cbac763a14bb4b46795068f793e2c31
Guilherme Santana <guilherme09augustoadams@gmail.com>
2023-05-07 12:51:57 -0400

    Migration da tabela Guard

---
+++ api/database/migrations/2023_05_07_153520_create_table_guard.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('guard', function (Blueprint $table) {
+            $table->id();
+            $table->string('guard_name', 50);
+            $table->string('registration', 15);
+            $table->string('password', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('guard');
+    }
+};
